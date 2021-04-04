4a1e7d635c176a09c97c0ccdf8541b0e8c7bc2f8
phanison898 <phanison898@gmail.com>
2021-04-04 22:18:16 +0530

    created template pages

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return """
-    <h1>Hello world</h1>
-    """
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
