36e65cd9565c3f42e7b6a08c863349e8fc1ff350
phanison898 <phanison898@gmail.com>
2021-04-04 23:39:26 +0530

    added post request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -8,7 +8,7 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200),nullable=False)
+    task_name = db.Column(db.String(200),nullable=False)
     date_created = db.Column(db.DateTime,default=datetime.utcnow)
 
     def __repr__(self):
@@ -16,7 +16,19 @@ class Todo(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        input_task_name = request.form['task-input']
+        new_todo_model = Todo(task_name=input_task_name)
+    
+        try:
+            db.session.add(new_todo_model)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error occured while creating new task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html',tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
